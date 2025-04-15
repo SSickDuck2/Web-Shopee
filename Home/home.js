7736f70536c5e8600fa32c9261ffce01ffba8a63
Wodnier <ducbao19122005@gmail.com>
2025-04-16 01:31:59 +0700

    Navigation bar prototype - commit

---
+++ Home/home.js
@@ -0,0 +1,4 @@
+document.getElementById("menu").addEventListener( "click", function (){
+    const dropdown = document.querySelector(".menudrop");
+    dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+});
